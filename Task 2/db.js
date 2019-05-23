bf11cab8b46f963f925f1c560dda95529224a406
OlegUrmanov <olegurmanov@mail.ru>
2019-05-23 17:20:46 +0300

    Added array to separate file and fixed server.js

---
+++ Task 2/db.js
@@ -0,0 +1,17 @@
+var weather = [{
+    id: 1,
+    city: 'Kazan',
+    temp: '+12',
+    humidity: '54%',
+    wind: '15 м/с'
+  },
+  {
+    id: 2,
+    city: 'Volzhsk',
+    temp: '+22',
+    humidity: '50%',
+    wind: '7 м/с'
+  }
+];
+
+module.exports = weather;
--- Task 2/server.js
+++ Task 2/server.js
@@ -2,33 +2,19 @@ var express = require('express');
 
 var app = express();
 
-var weather = [
-  {
-    city: 'Kazan',
-    temp: '+12',
-    humidity: '54%',
-    wind: '15 м/с'
-  },
-  {
-    city: 'Volzhsk',
-    temp: '+22',
-    humidity: '50%',
-    wind: '7 м/с'
-  }
-];
+var weather = require('./db.js');
 
-app.get('/weather', function(req, res){
-  res.send(weather);
-})
-
-app.get('/weather/:city', function(req, res){
+app.get('/weather/:id', function(req, res, next) {
   console.log(req.params);
-  var city_weather = weather.find(function (city_weather){
-    return city_weather.city === req.params.city
+  var city_weather = weather.find(function(city_weather) {
+    return city_weather.id === Number(req.params.id)
   });
+  if (city_weather == null || city_weather == 'undefined') {
+    res.status(404);
+  }
   res.send(city_weather);
 })
 
-app.listen(3012, function(){
+app.listen(3012, function() {
   console.log('API app started');
 })
